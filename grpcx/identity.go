48971425fe4287dd3e8d838f80d3b0fb819fe4a0
Oligarch E <>
2020-12-17 14:19:58 -0800

    Add grpcx error.go file real quick

--- grpcx/identity.go
+++ grpcx/identity.go
@@ -1,8 +1,6 @@
 package grpcx
 
 import (
-	"fmt"
-
 	"github.com/oligarch316/go-netx"
 	"github.com/oligarch316/go-netx/serverx"
 )
@@ -20,21 +18,6 @@ func (identity) ID() serverx.ServiceID { return ID }
 
 // ID TODO.
 var ID serverx.ServiceID = serviceID{}
-
-// Error TODO.
-type Error struct {
-	Component string
-	err error
-}
-
-func (e Error) Error() string {
-	return fmt.Sprintf("%s %s: %s", namespace, e.Component, e.err.Error())
-}
-
-// Unwrap TODO.
-func (e Error) Unwrap() error {
-	return e.err
-}
 
 // WithListeners TODO.
 func WithListeners(ls ...netx.Listener) serverx.Option {
