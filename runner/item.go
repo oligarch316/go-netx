4626dbba8d0015072d97479148617565f0860d54
oligarch316 <45413615+oligarch316@users.noreply.github.com>
2021-12-09 15:39:06 -0500

    Bugfix/double channel close (#2)
    * Quick rename because I can't help myself
    
    * Bug found and fixed
    - Was a scope issue in serverx/service.go
    - Clobbering the "item" loop var when wrapping multi listener runners
    - Silly
    
    Co-authored-by: Oligarch E <>

--- runner/item.go
+++ runner/item.go
@@ -8,16 +8,16 @@ type Item interface {
 	Close(context.Context) error
 }
 
-type runner struct {
+// New TODO.
+func New(runFunc func() error, closeFunc func(context.Context) error) Item {
+	return item{doRun: runFunc, doClose: closeFunc}
+}
+
+type item struct {
 	doRun   func() error
 	doClose func(context.Context) error
 }
 
-func (r runner) Run() error { return r.doRun() }
+func (i item) Run() error { return i.doRun() }
 
-func (r runner) Close(ctx context.Context) error { return r.doClose(ctx) }
-
-// New TODO.
-func New(runFunc func() error, closeFunc func(context.Context) error) Item {
-	return runner{doRun: runFunc, doClose: closeFunc}
-}
+func (i item) Close(ctx context.Context) error { return i.doClose(ctx) }
